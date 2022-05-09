b08c67af7d957e441dae573b72eeb3b472222f13
Crestwave <crest.wave@yahoo.com>
2022-05-09 11:30:58 +0800

    mods/predict-bloom: 2.0

--- mods/predict-bloom/modinfo.lua
+++ mods/predict-bloom/modinfo.lua
@@ -6,7 +6,7 @@ Displays a bloom meter for Wormwood, allowing you to easily manage and maintain 
 Compatible with Combined Status and Status Announcements.
 ]]
 
-version = "1.5"
+version = "2.0"
 api_version = 10
 
 dst_compatible = true
