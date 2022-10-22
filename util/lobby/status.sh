efbbf47e9253349db47cf14aa5c20914f06e235d
Crestwave <crest.wave@yahoo.com>
2022-10-22 10:21:01 +0800

    util/lobby: add status.sh

---
+++ util/lobby/status.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+./server.sh '~'
+./get-hosts.sh -v >serverlist2
+awk '
+	/[[:digit:]]+-[[:digit:]]+$/ {
+		split($NF, r, "-")
+		$NF = ""
+		for (i = r[1]; i <= r[2]; ++i)
+			print $0 i
+		next
+	}
+	
+	{ print }
+	' serverlist >grouplist
+
+latest=$(curl -s https://s3.amazonaws.com/dstbuilds/builds.json | jq '.release[-1] | tonumber')
+
+while read -r line; do
+	[[ $line == *Beta* ]] && continue
+	sv=$(grep ",\(\[󰀘\] \)\?$line," serverlist2 || echo "$line" DOWN >&2)
+	if [[ $sv ]]; then
+		IFS=, read -r host name region version group <<<"$sv"
+		if [[ $version != $latest ]]; then
+			echo "$line" OUTDATED
+		fi
+	fi
+done <grouplist
