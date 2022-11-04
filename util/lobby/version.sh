6cf19eada35ffd27adffbb9f9aa61457edd06b92
Crestwave <crest.wave@yahoo.com>
2022-11-04 08:25:26 +0800

    version.sh: update beta version detection

--- util/lobby/version.sh
+++ util/lobby/version.sh
@@ -2,12 +2,7 @@
 getopts b opt
 
 if [ "$opt" = b ]; then
-	curl -s 'https://forums.kleientertainment.com/game-updates/dst/' |
-		awk -F '[-/]' '
-			/class=.cRelease./ && !/data-currentRelease/ {
-				print $7
-				exit
-			}'
+	curl -s https://s3.amazonaws.com/dstbuilds/builds.json | jq '.updatebeta[-1] | tonumber'
 else
 	curl -s https://s3.amazonaws.com/dstbuilds/builds.json | jq '.release[-1] | tonumber'
 fi
