3537a2e3d5a2d803a2a3cf60940b71d406bd72b7
Crestwave <crestwave@users.noreply.github.com>
2022-11-05 23:24:19 +0800

    mods/maxwell-keybinds: init

---
+++ mods/maxwell-keybinds/modinfo.lua
@@ -0,0 +1,55 @@
+name = "Maxwell Keybinds"
+author = "Crestwave"
+description = [[
+Lets Maxwell use X to cast his Codex Umbra and R to access his Magician's Top Hat.
+
+Keybinds are configurable.
+]]
+
+version = "1.0"
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+
+client_only_mod = true
+server_only_mod = false
+all_clients_require_mod = false
+
+--[[
+TODO: Add icons
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+--]]
+
+configuration_options = {
+	{
+		name = "waxwelljournalkey",
+		label = "Codex Umbra Keybind",
+		hover = "Open the Codex's spell wheel",
+		options = {
+			--fill later
+		},
+		default = "X",
+	},
+	{
+		name = "tophatkey",
+		label = "Top Hat Keybind",
+		hover = "Open the Magician's Top Hat",
+		options = {
+			--fill later
+		},
+		default = "R",
+	},
+}
+
+local keys = {
+	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+	"LSHIFT","LALT","LCTRL","TAB","BACKSPACE","PERIOD","SLASH","TILDE",
+}
+
+for i=1, 2 do
+	for j=1, #keys do
+		configuration_options[i].options[j] = { description = keys[j], data = keys[j] }
+	end
+end
---
+++ mods/maxwell-keybinds/modmain.lua
@@ -0,0 +1,64 @@
+local _G = GLOBAL
+local waxwelljournalkey = _G["KEY_"..GetModConfigData("waxwelljournalkey")]
+local tophatkey = _G["KEY_"..GetModConfigData("tophatkey")]
+
+local function IsDefaultScreen()
+	return _G.ThePlayer ~= nil
+		and _G.ThePlayer.components.playercontroller:IsEnabled()
+		and _G.TheFrontEnd:GetActiveScreen().name == "HUD"
+end
+
+local function GetItem(prefab, tag)
+	local items = _G.ThePlayer.replica.inventory:GetItems()
+	local containers = _G.ThePlayer.replica.inventory:GetOpenContainers()
+
+	for k, v in pairs(_G.EQUIPSLOTS) do
+		table.insert(items, _G.ThePlayer.replica.inventory:GetEquippedItem(v))
+	end
+
+	for k, v in pairs(containers) do
+		_G.ConcatArrays(items, k.replica.container:GetItems())
+	end
+
+	for k, v in pairs(items) do
+		if v.prefab == prefab and (tag == nil or v:HasTag(tag)) then
+			return v
+		end
+	end
+end
+
+_G.TheInput:AddKeyDownHandler(waxwelljournalkey, function()
+	if IsDefaultScreen() then
+		-- TODO: Automatically refuel when empty
+		-- TODO: Add keybinds for picking spells
+		if _G.ThePlayer.HUD ~= nil and _G.ThePlayer.HUD:GetCurrentOpenSpellBook() ~= nil then
+			_G.ThePlayer.HUD:CloseSpellWheel()
+		else
+			local item = GetItem("waxwelljournal")
+
+			if item ~= nil then
+				for k, v in pairs(_G.ThePlayer.components.playeractionpicker:GetInventoryActions(item)) do
+					if v.action == _G.ACTIONS.USESPELLBOOK then
+						item.components.spellbook:OpenSpellBook(_G.ThePlayer)
+						_G.SendRPCToServer(_G.RPC.UseItemFromInvTile, _G.ACTIONS.USESPELLBOOK.code, item)
+					end
+				end
+			end
+		end
+	end
+end)
+
+_G.TheInput:AddKeyDownHandler(tophatkey, function()
+	if IsDefaultScreen() then
+		if _G.ThePlayer:HasTag("usingmagiciantool") then
+			local x, y, z = _G.ThePlayer.Transform:GetWorldPosition()
+			_G.SendRPCToServer(_G.RPC.RightClick, _G.ACTIONS.STOPUSINGMAGICTOOL.code, x, z, _G.ThePlayer)
+		else
+			local item = GetItem("tophat", "magiciantool")
+
+			if item ~= nil  then
+				_G.ThePlayer.replica.inventory:UseItemFromInvTile(item)
+			end
+		end
+	end
+end)
