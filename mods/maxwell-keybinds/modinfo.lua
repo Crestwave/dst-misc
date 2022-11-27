fc0bcb5c587e3d6d94aa976b17c728902f80e238
Crestwave <crestwave@users.noreply.github.com>
2022-11-27 13:16:26 +0800

    mods/maxwell-keybinds: add configuration for refuel thresholds and fix top hat access

--- mods/maxwell-keybinds/modinfo.lua
+++ mods/maxwell-keybinds/modinfo.lua
@@ -1,12 +1,12 @@
 name = "Maxwell Keybinds"
 author = "Crestwave"
 description = [[
-Lets Maxwell use X to cast or refuel his Codex Umbra and R to access his Magician's Top Hat.
+Lets Maxwell use X to cast/refuel his Codex Umbra and R to access his Magician's Top Hat.
 
 Keybinds are configurable.
 ]]
 
-version = "1.0"
+version = "1.1"
 api_version = 10
 
 dst_compatible = true
@@ -23,7 +23,7 @@ configuration_options = {
 	{
 		name = "waxwelljournalkey",
 		label = "Codex Umbra Keybind",
-		hover = "Open and refuels the Codex Umbra",
+		hover = "Open the Codex's spell wheel",
 		options = {
 			--fill later
 		},
@@ -38,15 +38,28 @@ configuration_options = {
 		},
 		default = "R",
 	},
+	{
+		name = "refuelpercent",
+		label = "Refuel Threshold",
+		hover = "Percentage threshold to refuel the Codex Umbra.",
+		options = {
+			--fill later
+		},
+		default = 75,
+	},
 }
 
 local keys = {
 	"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-	"LSHIFT","LALT","LCTRL","TAB","BACKSPACE","PERIOD","SLASH","TILDE",
+	"LSHIFT","LALT","LCTRL","TAB","BACKSPACE","PERIOD","SLASH","TILDE"
 }
 
 for i=1, 2 do
 	for j=1, #keys do
 		configuration_options[i].options[j] = { description = keys[j], data = keys[j] }
 	end
+end
+
+for i = 0, 100, 5 do
+	configuration_options[3].options[(i/5)+ 1] = { description = i.."%", data = i }
 end
--- mods/maxwell-keybinds/modmain.lua
+++ mods/maxwell-keybinds/modmain.lua
@@ -1,6 +1,7 @@
 local _G = GLOBAL
 local waxwelljournalkey = _G["KEY_"..GetModConfigData("waxwelljournalkey")]
 local tophatkey = _G["KEY_"..GetModConfigData("tophatkey")]
+local refuelpercent = GetModConfigData("refuelpercent")
 
 local function IsDefaultScreen()
 	return _G.ThePlayer ~= nil
@@ -29,14 +30,13 @@ end
 
 _G.TheInput:AddKeyDownHandler(waxwelljournalkey, function()
 	if IsDefaultScreen() then
-		-- TODO: Add keybinds for picking spells
 		if _G.ThePlayer.HUD ~= nil and _G.ThePlayer.HUD:GetCurrentOpenSpellBook() ~= nil then
 			_G.ThePlayer.HUD:CloseSpellWheel()
 		else
 			local item = GetItem("waxwelljournal")
 
 			if item ~= nil then
-				if item.replica.inventoryitem.classified.percentused:value() == 0 then
+				if item.replica.inventoryitem.classified.percentused:value() <= refuelpercent then
 					local fuel = GetItem("nightmarefuel")
 
 					if fuel ~= nil then
@@ -44,12 +44,12 @@ _G.TheInput:AddKeyDownHandler(waxwelljournalkey, function()
 					else
 						_G.ThePlayer.replica.inventory:InspectItemFromInvTile(item)
 					end
-				else
-					for k, v in pairs(_G.ThePlayer.components.playeractionpicker:GetInventoryActions(item)) do
-						if v.action == _G.ACTIONS.USESPELLBOOK then
-							_G.ThePlayer.replica.inventory:UseItemFromInvTile(item)
-							item.components.spellbook:OpenSpellBook(_G.ThePlayer)
-						end
+				end
+
+				for k, v in pairs(_G.ThePlayer.components.playeractionpicker:GetInventoryActions(item)) do
+					if v.action == _G.ACTIONS.USESPELLBOOK then
+						_G.ThePlayer.replica.inventory:UseItemFromInvTile(item)
+						item.components.spellbook:OpenSpellBook(_G.ThePlayer)
 					end
 				end
 			end
@@ -59,15 +59,12 @@ end)
 
 _G.TheInput:AddKeyDownHandler(tophatkey, function()
 	if IsDefaultScreen() then
-		if _G.ThePlayer:HasTag("usingmagiciantool") and (_G.ThePlayer.AnimState:IsCurrentAnimation("tophat_equipped_start") or _G.ThePlayer.AnimState:IsCurrentAnimation("tophat_loop")) then
+		if _G.ThePlayer:HasTag("usingmagiciantool") then
 			local x, y, z = _G.ThePlayer.Transform:GetWorldPosition()
 			_G.SendRPCToServer(_G.RPC.RightClick, _G.ACTIONS.STOPUSINGMAGICTOOL.code, x, z, _G.ThePlayer)
 		else
 			local item = GetItem("tophat", "magiciantool")
-
-			if item ~= nil  then
-				_G.ThePlayer.replica.inventory:UseItemFromInvTile(item)
-			end
+			_G.SendRPCToServer(_G.RPC.UseItemFromInvTile, _G.ACTIONS.USEMAGICTOOL.code, item, 1, nil)
 		end
 	end
 end)
