7a9507e966b581f59285d6896eb7a4e722d4e8c5
Crestwave <crestwave@users.noreply.github.com>
2023-03-22 10:21:56 +0800

    mods/compact-player-inspect: init

---
+++ mods/compact-player-inspect/modinfo.lua
@@ -0,0 +1,16 @@
+name = "Compact Player Inspect"
+author = "Crestwave"
+description = "Converts the player avatar popup to its pre-rework state when inspecting other players."
+
+version = "1.0"
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+
+client_only_mod = true
+server_only_mod = false
+all_clients_require_mod = false
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
---
+++ mods/compact-player-inspect/modmain.lua
@@ -0,0 +1,43 @@
+local _G = GLOBAL
+
+AddClassPostConstruct("screens/playerhud", function(self)
+	local PlayerAvatarPopup = require "widgets/_playeravatarpopup"
+	local DressupAvatarPopup = require "widgets/_dressupavatarpopup"
+
+	self._TogglePlayerInfoPopup = self.TogglePlayerInfoPopup
+	self.TogglePlayerInfoPopup = function(self, player_name, data, show_net_profile, force)
+		if data.userid ~= nil and data.userid == self.owner.userid then
+			return self:_TogglePlayerInfoPopup(player_name, data, show_net_profile, force)
+		end
+
+		if self.playeravatarpopup ~= nil and
+			self.playeravatarpopup.started and
+			self.playeravatarpopup.inst:IsValid() then
+			self.playeravatarpopup:Close()
+			if player_name == nil or
+				data == nil or
+				(data.userid ~= nil and self.playeravatarpopup.userid == data.userid) or --if we have a userid, test for that
+				(data.userid == nil and self.playeravatarpopup.target == data.inst) then --if no userid, then compare inst
+				self.playeravatarpopup = nil
+				return
+			end
+		end
+
+		if not force and _G.GetGameModeProperty("no_avatar_popup") then
+			return
+		end
+
+		-- Don't show steam button for yourself or targets without a userid(skeletons)
+		self.playeravatarpopup = self.controls.right_root:AddChild(
+			data.inst ~= nil and
+			data.inst:HasTag("dressable") and
+			DressupAvatarPopup(self.owner, player_name, data) or
+			PlayerAvatarPopup(self.owner, player_name, data, show_net_profile and data.userid ~= nil and data.userid ~= self.owner.userid)
+		)
+	end
+
+	self._IsPlayerInfoPopupOpen = self.IsPlayerInfoPopupOpen
+	self.IsPlayerInfoPopupOpen = function(...)
+		return self:IsPlayerAvatarPopUpOpen() or self:_IsPlayerPopupOpen()
+	end
+end)
