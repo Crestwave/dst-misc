19994a3aeba88e1dbd7848990a7becc019e1e259
Crestwave <crestwave@users.noreply.github.com>
2023-12-20 22:29:57 +0800

    mods/domestication-calculator: 1.1

--- mods/domestication-calculator/modinfo.lua
+++ mods/domestication-calculator/modinfo.lua
@@ -1,36 +1,16 @@
---The name of the mod displayed in the 'mods' screen.
 name = "Domestication Calculator"
-
---A description of the mod.
-description =
-[[
-Estimates a beefalo's domestication after being bucked off.
-]]
-
---Who wrote this awesome mod?
 author = "Crestwave"
+description = "Estimates a beefalo's domestication after getting bucked off."
 
---A version number so you can ask people if they are running an old version of your mod.
-version = "0.1"
-
---This lets other players know if your mod is out of date. This typically needs to be updated every time there's a new game update.
+version = "1.1"
 api_version = 10
 
 dst_compatible = true
 dont_starve_compatible = false
-reign_of_giants_compatible = false
-shipwrecked_compatible = false
 
---This determines whether it causes a server to be marked as modded (and shows in the mod list)
 client_only_mod = true
 server_only_mod = false
 all_clients_require_mod = false
 
---[[
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
---]]
-
-forumthread = ""
-
-configuration_options = {}
